72a71e73d6764b22413de44965546b711ec10d37
kral2 <cetin@hashicorp.com>
2023-05-25 11:48:13 +0200

    feat: creates an AWS VPC using a public registry module

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
+
 from constructs import Construct
-from cdktf import App, TerraformStack, CloudBackend, NamedCloudWorkspace
+from cdktf import App, TerraformStack, TerraformOutput, CloudBackend, NamedCloudWorkspace
+from cdktf_cdktf_provider_aws.provider import AwsProvider
+from imports.vpc import Vpc
 
-
-class MyStack(TerraformStack):
+class ModuleStack(TerraformStack):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
 
-        # define resources here
+        AwsProvider(self, "aws",
+            region = "eu-west-3",
+        )
 
+        vpc = Vpc(self, "MyCdktfPythonVpc",
+            name = "my-cdktf-python-vpc",
+            cidr = "10.0.0.0/16",
+            azs = ['eu-west-3a', 'eu-west-3b', 'eu-west-3c'],
+            private_subnets = ['10.0.1.0/24', '10.0.2.0/24', '10.0.3.0/24'],
+            public_subnets = ['10.0.101.0/24', '10.0.102.0/24', '10.0.103.0/24'],
+            enable_nat_gateway = False
+        )
+
+        TerraformOutput(self, "vpc_cidr",
+                        value=vpc.cidr,
+                        )
+        TerraformOutput(self, "public_subnet_arn",
+                        value=vpc.public_subnet_arns_output
+                        )
+        
 
 app = App()
-stack = MyStack(app, "cdktf-vpc")
-CloudBackend(stack,
+network_stack = ModuleStack(app, "cdktf-python-vpc-module")
+
+CloudBackend(network_stack,
   hostname='app.terraform.io',
   organization='hashikral',
-  workspaces=NamedCloudWorkspace('cdktf-vpc')
+  workspaces=NamedCloudWorkspace('cdktf-python-vpc')
 )
 
 app.synth()
